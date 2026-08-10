78e189e595efd4a9d6a7ca49485c14bb01f11727
Masud Ibn Musa <masudibnmusa10@gmail.com>
2026-08-10 23:50:16 +0600

    Adding src/services/menuService.js

---
+++ src/services/menuService.js
@@ -0,0 +1,10 @@
+import api from './api'
+
+export const menuService = {
+  getAll: (params) => api.get('/menu', { params }),
+  getById: (id) => api.get(`/menu/${id}`),
+  create: (data) => api.post('/menu', data),
+  update: (id, data) => api.put(`/menu/${id}`, data),
+  delete: (id) => api.delete(`/menu/${id}`),
+  getCategories: () => api.get('/menu/categories'),
+}
